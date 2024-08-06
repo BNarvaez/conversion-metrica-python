82e4f48a36868fed36041b9126f3329ccc9ebef2
Brayan Narvaez <brayannsaez77@gmail.com>
2024-08-06 15:24:51 -0500

    Realizamos una solucion en python para la conversion metrica de una empresa industrial

---
+++ conversion-metrica.py
@@ -0,0 +1,39 @@
+
+medida_en_cms = float(input("Por favor, ingresar las medidas de la pieza del mueble (en cms): "))
+
+medida_en_pulgada = medida_en_cms / 2.54
+
+print("Las medidas en pulgadas de la pieza ingresada son:", medida_en_pulgada)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
